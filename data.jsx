c07b16327d0abb85b3a8fb2eb3af90d90d501a21
Lukasz Leszczynski <lukasz.leszczynski95@gmail.com>
2024-01-28 01:14:50 +0100

    Setup

---
+++ data.jsx
@@ -0,0 +1,75 @@
+const menu = [
+    {
+        id: 1,
+        title: 'buttermilk pancakes',
+        category: 'breakfast',
+        price: 15.99,
+        img: 'https://images.unsplash.com/photo-1567620905732-2d1ec7ab7445?q=80&w=1980&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        desc: 'I am baby woke milkshake wolf bitters live-edge blue faBottleDroplet, hammock freegan copper mug whatever cold-pressed'
+    },
+   
+    {
+        id: 2,
+        title: 'buttermilk pancakes',
+        category: 'lunch',
+        price: 15.99,
+        img: 'https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?q=80&w=1981&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        desc: 'I am baby woke milkshake wolf bitters live-edge blue faBottleDroplet, hammock freegan copper mug whatever cold-pressed'
+    },
+   
+    {
+        id: 3,
+        title: 'buttermilk pancakes',
+        category: 'shakes',
+        price: 15.99,
+        img: 'https://images.unsplash.com/photo-1577805947697-89e18249d767?q=80&w=1898&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        desc: 'I am baby woke milkshake wolf bitters live-edge blue faBottleDroplet, hammock freegan copper mug whatever cold-pressed'
+    },
+   
+    {
+        id: 4,
+        title: 'buttermilk pancakes',
+        category: 'shakes',
+        price: 15.99,
+        img: 'https://images.unsplash.com/photo-1572490122747-3968b75cc699?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        desc: 'I am baby woke milkshake wolf bitters live-edge blue faBottleDroplet, hammock freegan copper mug whatever cold-pressed'
+    },
+   
+    {
+        id: 5,
+        title: 'buttermilk pancakes',
+        category: 'lunch',
+        price: 15.99,
+        img: 'https://images.unsplash.com/photo-1505576633757-0ac1084af824?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        desc: 'I am baby woke milkshake wolf bitters live-edge blue faBottleDroplet, hammock freegan copper mug whatever cold-pressed'
+    },
+   
+    {
+        id: 6,
+        title: 'buttermilk pancakes',
+        category: 'lunch',
+        price: 15.99,
+        img: 'https://plus.unsplash.com/premium_photo-1663858367029-442eaf365f8e?q=80&w=1915&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        desc: 'I am baby woke milkshake wolf bitters live-edge blue faBottleDroplet, hammock freegan copper mug whatever cold-pressed'
+    },
+   
+    {
+        id: 7,
+        title: 'buttermilk pancakes',
+        category: 'breakfast',
+        price: 15.99,
+        img: 'https://images.unsplash.com/photo-1482049016688-2d3e1b311543?q=80&w=1910&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        desc: 'I am baby woke milkshake wolf bitters live-edge blue faBottleDroplet, hammock freegan copper mug whatever cold-pressed'
+    },
+   
+    {
+        id: 8,
+        title: 'buttermilk pancakes',
+        category: 'breakfast',
+        price: 15.99,
+        img: 'https://images.unsplash.com/photo-1493770348161-369560ae357d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        desc: 'I am baby woke milkshake wolf bitters live-edge blue faBottleDroplet, hammock freegan copper mug whatever cold-pressed'
+    },
+   
+]
+export default menu
